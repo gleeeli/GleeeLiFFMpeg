2ac7e8883082cb6dc33820fce23bcae5c173eade
gleeeli <gleeeli@sina.com>
2018-06-28 16:39:13 +0800

    Initial Commit

---
+++ GleeeLiFFMpeg/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GleeeLiFFMpeg
+//
+//  Created by zqh on 2018/6/28.
+//  Copyright © 2018年 gleeli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GleeeLiFFMpeg/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GleeeLiFFMpeg
+//
+//  Created by zqh on 2018/6/28.
+//  Copyright © 2018年 gleeli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
